5e42e4e2f42fa8a22cd0d70de9b6afc17c3912df
Dariusz Ruszkiewicz <infoixnet@gmail.com>
2019-06-12 13:36:35 +0200

    git commit -a don't set new files as tracking git files :-(

---
+++ main.js
@@ -0,0 +1,12 @@
+//alert("co jest?");
+console.log('siema');
+const name = 'maciek';
+let isHappy = false;
+console.log(name, isHappy);
+console.log(`nazywam się ${name} 
+i to nie jest ${isHappy}`);
+
+console.log('nazywam się ' + name + ' i to nie jest ' +
+   isHappy + '-> stary sposób');
+
+console.log(name[0], name[1]);
